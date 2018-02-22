af3ecabdc21b4efc2748171e001832369dcf2542
mdekauwe <mdekauwe@gmail.com>
2018-02-22 11:45:33 +1100

    Further code additions

--- ogle_SAM.R
+++ ogle_SAM.R
@@ -61,10 +61,10 @@ model {
 
   for (i in 1:N) {
     # Data model (or likelihood) for the observed NPP data:
-    NPP[i] <- dnorm(mu[i], tau)
+    NPP[i] ~ dnorm(mu[i], tau)
 
     # Generate “replicated data” to evaluate model fit.
-    NPP_rep[i] <- dnorm(mu[i], tau)
+    NPP_rep[i] ~ dnorm(mu[i], tau)
 
     # Define model for latent (mean) NPP; Event[,k] represents the amount
     # of precipitation received in different size classes, where k indexes
@@ -74,6 +74,49 @@ model {
     mu[i] <- ( a[1] + (a[2] * antX[df2$YearID[i]] * INCH_TO_MM) +
               (a[3] * df2$Event[i,1]) + (a[4] * df2$Event[i,2]) +
               (a[5] * df2$Event[i,3]) + (a[6] * df2$Event[i,4]) )
+
+    # Some of the precipitation event data are missing, so specify a simple
+    # data model for the Event data for the purpose of estimating the
+    # missing data:
+    for (k in 1:4) {
+      Event[i,k] ~ dnorm(mu_ev[k], tau_ev[k])
+    }
+  }
+
+  # Dirichlet prior for monthly precipitation weights (due to restrictions
+  # on when the built-in dirichlet distribution can be used, we are required
+  # to use the relationship between the gamma distribution and the dirichlet
+  # to assign the dirichlet prior. For each time block into the past, assign
+  # the unnormalized weight (deltaX) a gamma(1,1) prior:
+  for (j in 1:Nblocks) {
+    deltaX[j] ~ dgamma(1,1)
+  }
+
+  for (t in 1:Nlag) {
+    # Compute the yearly weights:
+    yr_w[t] <- sum(weight[,t])
+    alphad[t] <- 1
+    for (m in 1:12){
+      # Redefine the unnormalized monthly weights to account for post-ANPP
+      # harvest period; i.e., 2nd part involving equals and step functions
+      # sets weight = 0 if in year 1 and in Oct, Nov, or Dec
+      # (i.e., post- ANPP harvest).
+      delta[m,t] <- (deltaX[block[t,m]]) * (1-equals(t,1) * step(m-9.5))
+
+      # Compute normalized monthly weights, which will be between 0 and 1,
+      # and will some to one.
+      weight[m,t] <- delta[m,t] / sumD
+
+      # Reorder the weights to go from most recent month (Dec of current
+      # year) to “oldest” month (Jan at past year = Nlag).
+      weightOrdered[(t-1)*12 + (12-m+1)] <- weight[m,t]
+
+      # For each time into the past, compute the weighted precipitation
+      # variable.
+      for (i in Nlag:Nyrs) {
+        antX1[i,m,t] <- weight[m,t]*ppt[i-t+1,m]
+      }
+    }
   }
 
   # Compute sum of deltas (unnormalized weights), to be used to compute
@@ -112,18 +155,18 @@ model {
 
   # Assign priors to the ANPP regression parameters (covariate effects):
   for (k in 1:6) {
-    a[k] <- dnorm(0, 1E-07)
+    a[k] ~ dnorm(0, 1E-07)
   }
 
   # Prior for residual (observation) standard deviation, and compute
   # associated precision:
-  sig <- dunif(0, 100)
+  sig ~ dunif(0, 100)
   tau <- pow(sig,-2)
 
   # Priors for parameters in the Event missing data model:
   for (k in 1:4) {
-    mu_ev[k] <- dunif(0, 500)
-    sig_ev[k] <- dunif(0, 500)
+    mu_ev[k] ~ dunif(0, 500)
+    sig_ev[k] ~ dunif(0, 500)
     tau_ev[k] <- pow(sig_ev[k], -2)
   }
 
