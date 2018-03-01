2dd7c5157e81ad7711e2a16979f7588be4110aa2
mdekauwe <mdekauwe@gmail.com>
2018-03-01 15:31:28 +1100

    Added chain plotting script

--- plot_chains.R
+++ plot_chains.R
@@ -1,54 +1,40 @@
 #!/usr/bin/Rscript
 
-# Plot the MCMC chains
+# Plot the chains to check on mixing ...
 #
 # Author: Martin De Kauwe
 # Email: mdekauwe@gmail.com
-# Date: 22.02.2018
+# Date: 01.03.2018
 
-library(rjags)
 library(ggplot2)
 library(cowplot)
+
 
 wd <- getwd()
 setwd(wd)
 
-nchains <- 4
-samples <- 10000
-
-samples1 <- read.csv(file=paste("outputs/samples_iter_1_to_", samples,
+chain1 <- read.csv(file=paste("outputs/chain_iter_1_to_", chain,
                      "_chain1.csv", sep=""), header=TRUE)
-samples2 <- read.csv(file=paste("outputs/samples_iter_1_to_", samples,
+chain2 <- read.csv(file=paste("outputs/chain_iter_1_to_", chain,
                      "_chain2.csv", sep=""), header=TRUE)
-samples3 <- read.csv(file=paste("outputs/samples_iter_1_to_", samples,
+chain3 <- read.csv(file=paste("outputs/chain_iter_1_to_", chain,
                      "_chain3.csv", sep=""), header=TRUE)
-samples4 <- read.csv(file=paste("outputs/samples_iter_1_to_", samples,
+chain4 <- read.csv(file=paste("outputs/chain_iter_1_to_", chain,
                      "_chain4.csv", sep=""), header=TRUE)
 
+samples <- 50000 # samples to be kept after burn in
 
-end <- dim(samples1)[1]
-start <- (end / 2) + 1
-col = 8
+png(filename="plots/chains.png")
 
-# Calculate the deviance of the final 5000 iterations of each chain.
-# If deviance diff > 5, then need to reinitialize.
-mu_dev <- c(mean(samples1[start:end,col]), mean(samples2[start:end,col]),
-            mean(samples3[start:end,col]), mean(samples4[start:end,col]))
-min_dev <- c(min(samples1[start:end,col]), min(samples2[start:end,col]),
-             min(samples3[start:end,col]), min(samples4[start:end,col]))
-max_dev <- c(max(samples1[start:end,col]), max(samples2[start:end,col]),
-            max(samples3[start:end,col]), max(samples4[start:end,col]))
+for (i in 1:6) {
 
-print(mu_dev)
-print(min_dev)
-print(max_dev)
+  plot(c(1:samples), chain1[,i+1], type='l', col='black',
+       main=paste("alpha",i), xlab="iteration no.",
+       ylab=paste("alpha",i))
+  points(c(1:samples), chain2[,i+1], type='l', col='blue')
+  points(c(1:samples), chain3[,i+1], type='l', col='green')
+  points(c(1:samples), chain4[,i+1], type='l', col='red')
 
-print(samples1["mu.1."])
+}
 
-hist(samples1["mu.1."])
-#for (v in 1:nvar(samples1)) {
-#
-#  print(samples1[,v])
-#
-#
-#}
+dev.off()
