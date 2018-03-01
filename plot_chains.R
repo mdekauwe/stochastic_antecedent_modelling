8e9e4294ea03001f90d3242bb817d03f9b25d3e9
mdekauwe <mdekauwe@gmail.com>
2018-03-01 15:54:51 +1100

    Fixed plotting script

--- plot_chains.R
+++ plot_chains.R
@@ -13,27 +13,35 @@ library(cowplot)
 wd <- getwd()
 setwd(wd)
 
-chain1 <- read.csv(file=paste("outputs/chain_iter_1_to_", chain,
+samples <- 50000 # samples to be kept after burn in
+thin <- 10
+N <- samples / thin
+
+chain1 <- read.csv(file=paste("outputs/chain_iter_1_to_", samples,
                      "_chain1.csv", sep=""), header=TRUE)
-chain2 <- read.csv(file=paste("outputs/chain_iter_1_to_", chain,
+chain2 <- read.csv(file=paste("outputs/chain_iter_1_to_", samples,
                      "_chain2.csv", sep=""), header=TRUE)
-chain3 <- read.csv(file=paste("outputs/chain_iter_1_to_", chain,
+chain3 <- read.csv(file=paste("outputs/chain_iter_1_to_", samples,
                      "_chain3.csv", sep=""), header=TRUE)
-chain4 <- read.csv(file=paste("outputs/chain_iter_1_to_", chain,
+chain4 <- read.csv(file=paste("outputs/chain_iter_1_to_", samples,
                      "_chain4.csv", sep=""), header=TRUE)
 
-samples <- 50000 # samples to be kept after burn in
+chain1 <- as.matrix(chain1)
+chain2 <- as.matrix(chain2)
+chain3 <- as.matrix(chain3)
+chain4 <- as.matrix(chain4)
+
 
 png(filename="plots/chains.png")
 
 for (i in 1:6) {
 
-  plot(c(1:samples), chain1[,i+1], type='l', col='black',
+  plot(c(1:N), chain1[,i+1], type='l', col='black',
        main=paste("alpha",i), xlab="iteration no.",
        ylab=paste("alpha",i))
-  points(c(1:samples), chain2[,i+1], type='l', col='blue')
-  points(c(1:samples), chain3[,i+1], type='l', col='green')
-  points(c(1:samples), chain4[,i+1], type='l', col='red')
+  points(c(1:N), chain2[,i+1], type='l', col='blue')
+  points(c(1:N), chain3[,i+1], type='l', col='green')
+  points(c(1:N), chain4[,i+1], type='l', col='red')
 
 }
 
